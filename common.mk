f4d16c93df3344da266d8c29ff3e630fbd5bc6e8
Arian <arian.kulmer@web.de>
2025-02-08 10:22:01 +0530

    sm8450-common: sensors: Implement AOD notifier
    [adithya2306]:
    Stripped from complete sensor-notifier to include only aod notifier.
    
    Change-Id: I2ba35d7b7db4e6922d83f041497fe66bf52124cb
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- common.mk
+++ common.mk
@@ -387,7 +387,8 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal \
-    libsensorndkbridge
+    libsensorndkbridge \
+    sensor-notifier
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
