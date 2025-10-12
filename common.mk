812d0e657823931a1ca372793ab08661ed1aff42
ShadeDark6 <nizamfa6@gmail.com>
2025-10-12 09:12:06 +0000

    sm8450-common: Add powerhint support

--- common.mk
+++ common.mk
@@ -323,11 +323,11 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-android.hardware.power-service-qti \
-android.hardware.power@1.2.vendor \
+    android.hardware.power-service-qti \
+    android.hardware.power@1.2.vendor \
 
 PRODUCT_COPY_FILES += \
-vendor/qcom/opensource/power/config/taro/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Platform
 TARGET_BOARD_PLATFORM := taro
